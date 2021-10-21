e878a625de7dcdb85e4cd2174f535aa5b7424a5d
Forasky <artem8m@mail.ru>
2021-10-21 21:25:37 +0300

    adding some content
    - added BottomNavigatorBar with 3 routes

---
+++ lib/pages/widgets/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Some on Home page', textAlign: TextAlign.center,));
+  }
+}
